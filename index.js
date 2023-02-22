88e335aaf76604147d19ac144875b44c7fa8da86
dsuarez1 <dsuarez@robinfood.com>
2023-02-21 19:52:51 -0500

    firstcommit1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const token = process.env['token'];
 const client = new Client({
   intents: ['GuildMessages', 'Guilds']
 });
+let lastMessage = null;
 
 const targetUserId = '549966042185859085'; // ID del usuario al que se responderá
 
@@ -10,11 +11,14 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async (message) => {
   // Verificar si el mensaje fue enviado por el usuario objetivo
   if (message.author.id === targetUserId) {
+    if (lastMessage) {
+        await lastMessage.delete().catch(console.error);
+      }
     // Responder al mensaje del usuario objetivo
-    message.reply('https://media.discordapp.net/attachments/947267307648974928/1077686980277706762/Faeox49X0AA1Jf7.png');
+    lastMessage = await message.reply('https://media.discordapp.net/attachments/947267307648974928/1077686980277706762/Faeox49X0AA1Jf7.png');
   }
 });
 
